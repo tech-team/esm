a7713ca3b830e13bfa7cce67d3433ddd7850cfc1
igorcoding <igorcoding@gmail.com>
2015-03-02 23:00:55 +0300

    More of The Api

--- routes/api_editor.js
+++ routes/api_editor.js
@@ -5,8 +5,12 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Model = mongoose.model('model');
 
-function validateModel(model) {
+function validateModel(model, checkForId) {
     if (!model || model == {}) {
+        return false;
+    }
+
+    if (checkForId && !_.isString(model['_id'])) {
         return false;
     }
 
@@ -18,7 +22,18 @@ function validateModel(model) {
     return v;
 }
 
-var RESP = {
+function configureResp(responses) {
+    var newRESP = {};
+    _.forEach(responses, function(r, key) {
+        newRESP[key] = function(extension) {
+            extension = extension || {};
+            return _.extend(_.clone(r), extension);
+        }
+    });
+    return newRESP;
+}
+
+var RESP = configureResp({
     ok: {
         status: 200,
         msg: 'ok'
@@ -29,11 +44,31 @@ var RESP = {
         msg: 'Provided model is not valid'
     },
 
+    modelIdIsRequired: {
+        status: 402,
+        msg: 'Model id is required'
+    },
+
     modelSavingError: {
         status: 501,
         msg: 'Error happened while saving a model'
+    },
+
+    modelUpdatingError: {
+        status: 502,
+        msg: 'Error happened while updating a model'
+    },
+
+    modelNotFound: {
+        status: 404,
+        msg: 'Requested model not found'
+    },
+
+    modelsSelectingError: {
+        status: 503,
+        msg: 'Error happened while selecting models'
     }
-};
+});
 
 router.post('/model', function(req, res, next) {
     var model = req.body;
@@ -41,19 +76,82 @@ router.post('/model', function(req, res, next) {
         Model.create(model, function(err, saved_model) {
             if (err) {
                 console.error("Error while saving model: ", err);
-                res.status(500).json(RESP.modelSavingError);
+                res.status(500).json(RESP.modelSavingError());
                 return;
             }
 
-            res.json(_.extend(RESP.ok, {
-                model_id: saved_model._id
+            res.json(RESP.ok({
+                _id: saved_model._id
             }));
         });
 
 
     } else {
-        res.status(400).json(RESP.invalidModel);
+        res.status(400).json(RESP.invalidModel());
     }
+});
+
+router.put('/model', function(req, res, next) {
+    var model = req.body;
+    if (validateModel(model, true)) {
+        var id = model._id;
+        delete model._id;  // or mongo will try to save id as String
+
+        Model.update(id, model, { upsert: false }, function(err) {
+            if (err) {
+                console.error("Error while updating model: ", err);
+                res.status(500).json(RESP.modelUpdatingError());
+                return;
+            }
+
+            res.json(RESP.ok());
+        });
+
+
+    } else {
+        res.status(400).json(RESP.invalidModel());
+    }
+});
+
+router.get('/model', function(req, res, next) {
+    var model_id = req.query.id;
+    if (model_id) {
+        Model.findOne({ _id: model_id }, function(err, model) {
+            if (err) {
+                console.error("Error while selecting: ", err);
+                res.status(500).json(RESP.modelsSelectingError());
+                return;
+            }
+
+            if (model) {
+                res.json(RESP.ok({
+                    model: model
+                }));
+            } else {
+                res.status(404).json(RESP.modelNotFound());
+            }
+        });
+    } else {
+        res.status(400).json(RESP.modelIdIsRequired());
+    }
+});
+
+router.get('/model/list', function(req, res, next) {
+    Model.find({}, { _id: true, name: true }, function(err, models) {
+        if (err) {
+            console.error("Error while selecting: ", err);
+            res.status(500).json(RESP.modelsSelectingError());
+            return;
+        }
+
+        if (!models) {
+            models = [];
+        }
+
+        res.json(RESP.ok({
+            models: models
+        }));
+    });
 });
 
 
