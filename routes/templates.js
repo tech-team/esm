d31efefce2180ab3ddf6c3f30a338c1490e38541
alekseyl1992 <alekseyl@list.ru>
2015-03-15 01:49:37 +0300

    Partials - the way to DRY

--- routes/templates.js
+++ routes/templates.js
@@ -3,28 +3,50 @@ var router = express.Router();
 
 var modelsInteractor = require('./api/ModelsInteractor');
 
+var partials = {
+    pageTitle: 'pageTitle'
+};
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        partials: partials,
+        title: 'index'
+    });
 });
 
 router.get('/editor', function(req, res, next) {
-    res.render('editor', { title: 'Express' });
+    res.render('editor', {
+        partials: partials,
+        title: 'editor'
+    });
 });
 
 router.get('/client', function(req, res, next) {
-    res.render('client', { title: 'Express' });
+    res.render('client', {
+        partials: partials,
+        title: 'client'
+    });
 });
 
 router.get('/poll', function(req, res, next) {
-    res.render('poll', { title: 'Express' });
+    res.render('poll', {
+        partials: partials,
+        title: 'poll'
+    });
 });
 
 router.get('/report', function(req, res, next) {
-    res.render('report', { title: 'Express' });
+    res.render('report', {
+        partials: partials,
+        title: 'report'
+    });
 });
 
 router.get('/objectsManager', function(req, res, next) {
-    res.render('objectsManager', { title: 'Express' });
+    res.render('objectsManager', {
+        partials: partials,
+        title: 'objects manager'
+    });
 });
 
 module.exports = router;
